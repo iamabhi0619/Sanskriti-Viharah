f3a862edebe66612643e86ed2ab2ad8d854d4edd
Rohit Gusain <98213628+ItachiUchiha7777@users.noreply.github.com>
2024-09-13 23:59:37 +0530

    starting project

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/data", (req, res) => {
+  res.send(`Received data: ${JSON.stringify(req.body)}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
